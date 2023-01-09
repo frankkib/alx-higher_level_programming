4b9e3bcf03de647828392e7e375e4754aceeba5f
frankkib <frankkibagendi13@gmail.com>
2023-01-09 12:08:12 +0000

    printing new string from missing characters

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+#!/urs/bin/python3
+def no_c(my_string):
+    new_string = ""
+    for num in range(len(my_string)):
+        if my_string[num] != "cC"
+        new_string += my_string[num]
+        return new_string
