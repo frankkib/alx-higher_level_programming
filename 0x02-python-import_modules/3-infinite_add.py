41a48cf309651fc68d27a538c3d2c3df564f219c
frankkib <frankkibagendi13@gmail.com>
2023-01-10 11:55:35 +0000

    printing reuslt of all arguements

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-import hidden_4
 if __name__ == "__main__":
-    list = dir(hidden_4)
-    for j in range(len(list)):
-        if(list[j][0] != '_' and list[j][1] != '_'):
-            print('{}'.format(list[j]))
+     import sys
+     sum_total = 0
+     for i in range(len(sys.argv) - 1):
+         sum_total += int(sys.argv[i + 1])
+        print("{}".format(sum_total))
