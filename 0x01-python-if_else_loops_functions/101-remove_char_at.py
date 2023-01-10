51967411b4a9316c2e2864baa7b476a31a56292b
frankkib <frankkibagendi13@gmail.com>
2023-01-10 09:20:59 +0000

    removing a string

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    new_string = ''
+    for i in range(len(str)):
+        if num == n:
+            continue
+        new_string += str[num]
+        return (new_string)
