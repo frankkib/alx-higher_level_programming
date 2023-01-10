ca2b8849bd81e3cdd8b1da60c5f066a170542df0
frankkib <frankkibagendi13@gmail.com>
2023-01-10 11:59:27 +0000

    printing number of arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,23 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-     import sys
-     sum_total = 0
-     for i in range(len(sys.argv) - 1):
-         sum_total += int(sys.argv[i + 1])
-        print("{}".format(sum_total))
+from sys import argv
+
+
+def print_sys_arguments():
+    """prints the number of and the list of its arguments
+    Args:
+        No arguments
+    Returns:
+        The return value. Nothing
+    """
+    ac = len(argv)
+    print("{:d} {:s}{:s}{:s}".format(
+        ac - 1,
+        "argument" if ac <= 2 else "arguments",
+        "s" if ac <= 1 else "",
+        "." if ac == 1 else ":"))
+    for index, args in enumerate(argv):
+        if index == 0:
+            continue
+        print("{:d}: {:s}".format(index, args))
+        if __name__ == "__main__":
+            print_sys_arguments()
