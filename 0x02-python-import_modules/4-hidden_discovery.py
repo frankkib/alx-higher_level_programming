af63e51fa76c8418027248e00376d30c9ff98169
frankkib <frankkibagendi13@gmail.com>
2023-01-10 11:54:11 +0000

    program that imports functions

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-from variable_load_5 import a
-def print_variable():
-        print(a)
-
-        if __name__ == "__main__":
-             print_variable()
+import hidden_4
+if __name__ == "__main__":
+    list = dir(hidden_4)
+    for j in range(len(list)):
+        if(list[j][0] != '_' and list[j][1] != '_'):
+            print('{}'.format(list[j]))
