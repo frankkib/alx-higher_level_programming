c969a82b0f8fc8599a102ec58963fa8515e48055
frankkib <frankkibagendi13@gmail.com>
2023-01-11 09:59:43 +0000

    replacing my list

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    if not my_list:
+        return my_list
+    new_list = []
+    for number, new_number in enumerate(new_list):
+        if new_number == search:
+            new_list[number] = replace
+            return new_list
+        else:
+            return my_list
