28fe646dbe5ed81c31d5c2be8aa794a8205c77d4
frankkib <frankkibagendi13@gmail.com>
2023-01-16 11:32:38 +0000

    adding two numbers

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-from add_0 import add
-def print_addition():
-a = 1
-b = 2
-print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-if __name__ == "__main__":
-    print_addition():
+if _name_ == "_main_":
+    from add_0 import add
+
+    a = 1
+    b = 2
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
