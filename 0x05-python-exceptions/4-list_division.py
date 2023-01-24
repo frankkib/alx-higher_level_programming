1a1838c6d9c7cdb0a062efed8a5b9441781e26af
frankkib <frankkibagendi13@gmail.com>
2023-01-24 07:24:22 +0000

    function that divides two elements of a list

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    new_list = []
+    result = 0
+    for num in range(list_length):
+        try:
+            result = my_list_1[num] / my_list_2[num]
+        except (ZeroDivisionError):
+            print("division by 0")
+            continue
+        except (IndexError):
+            print("out of range")
+            continue
+        except (TypeError)
+        print("wrong type")
+        finally:
+            new_list.append(result)
+            return (new_list)
