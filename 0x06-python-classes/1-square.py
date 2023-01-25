64d3eded4c2ae3f67f7ac8b9144e3cbc8cc8e406
frankkib <frankkibagendi13@gmail.com>
2023-01-25 09:05:45 +0000

    class defination and initialization

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+class Square:
+    """class defination"""
+
+    def _init_(self, size):
+
+        """class initialization
+        Args:
+            size (int): size of the square
+        """
+        self._size = size
