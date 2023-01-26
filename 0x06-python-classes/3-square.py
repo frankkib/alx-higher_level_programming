c5ddd1039f74d1aed24b95eca4484f94d4971779
frankkib <frankkibagendi13@gmail.com>
2023-01-26 08:08:35 +0000

    fixed the attribute error

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 """class defination"""
+
+
 class Square:
     """class initialization"""
     def __init__(self, size=0):
-        if(type(size) is not int):
+        if (type(size) is not int):
             raise TypeError("size must be an integer")
-        elif(size < 0):
+        elif (size < 0):
             raise ValueError("size must be >= 0")
         self.__size = size
-        def area(self):
-            return self.__size ** 2
+
+    def area(self):
+        return (self.__size * self.__size)
