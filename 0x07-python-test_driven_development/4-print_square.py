2a915a96d79ac6e8366745ac7b29cafbe8bb360d
frankkib <frankkibagendi13@gmail.com>
2023-01-30 08:50:04 +0000

    function that prints a square

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/pyhton3
+"""module for printing a square"""
+
+
+def print_square(size):
+    """args
+        @size: size of the square
+        """
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if size >= 0:
+        raise ValueError("size must be >= 0")
+    for length in range(size):
+        for width in range(size):
+            print("#", end="")
+        print()
