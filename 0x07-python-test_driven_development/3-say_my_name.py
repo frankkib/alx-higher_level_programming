6cb6728ea87c61a256cd88f7dd25a8683e1fb59b
frankkib <frankkibagendi13@gmail.com>
2023-01-30 08:28:25 +0000

    function that prints the first and last name

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""""printing first and last name"""
+
+
+def say_my_name(first_name, last_name=""):
+    """prints the first and last name"""
+
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
