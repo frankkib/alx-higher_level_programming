83035e0d2dd9e69dedc99401ee1a45f051ce4e80
frankkib <frankkibagendi13@gmail.com>
2023-01-31 11:14:37 +0000

    function that prevents user fromadding attributes

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" class defination module"""
+
+
+
+class LockedClass:
+    """ prevents the user from dynamically creating new instance attributes"""
+       __slots__ = ['first_name']
