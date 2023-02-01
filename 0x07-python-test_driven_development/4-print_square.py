38a9142508c90864b5a81e51e2e77bd20654b7d0
frankkib <frankkibagendi13@gmail.com>
2023-02-01 08:16:01 +0000

    fixed the size error

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -8,7 +8,7 @@ def print_square(size):
         """
     if type(size) is not int:
         raise TypeError("size must be an integer")
-    if size >= 0:
+    if size < 0:
         raise ValueError("size must be >= 0")
     for length in range(size):
         for width in range(size):
