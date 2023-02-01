d92450ebaab3f50137918945b68c5e27f43ba66b
frankkib <frankkibagendi13@gmail.com>
2023-02-01 08:38:44 +0000

    fixed functions error

--- 0x07-python-test_driven_development/6-max_integer.py
+++ 0x07-python-test_driven_development/6-max_integer.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
-"""module for finding maximum number"""
+"""Module to find the max integer in a list
+"""
 
 
 def max_integer(list=[]):
+    """Function to find and return the max integer in a list of integers
+        If the list is empty, the function returns None
+    """
     if len(list) == 0:
         return None
-    max_number = list[0]
-    index = 1
-    while index < len(list):
-        if list[index] > max_number:
-            max_number = list[index]
-            index += 1
-    return max_number
+    result = list[0]
+    i = 1
+    while i < len(list):
+        if list[i] > result:
+            result = list[i]
+        i += 1
+    return result
+
