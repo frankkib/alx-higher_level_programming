d76240b1fa7dc667fe484310eea1c5d13af90899
frankkib <frankkibagendi13@gmail.com>
2023-02-02 07:02:14 +0000

    function that import a variable and prints it

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 from variable_load_5 import a
-def print_variable():
-        print(a)
 
-        if __name__ == "__main__":
-             print_variable()
+
+def print_variable():
+    print(a)
+
+
+if __name__ == "__main__":
+    print_variable()
