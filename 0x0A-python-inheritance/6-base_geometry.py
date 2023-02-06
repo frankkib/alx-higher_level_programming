e6780e2888ab6d16f8472851b48b4e5a6020e21e
frankkib <frankkibagendi13@gmail.com>
2023-02-06 09:42:44 +0000

    finction that raises an exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""BaseGeometry class defination"""
+
+
+class BaseGeometry:
+    """BaseGeometry class"""
+
+    def area(self):
+        """raises an exception"""
+        raise Exception("area() is not implemented")
