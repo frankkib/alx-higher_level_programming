be54f3436420a6b70e558ca08d7064ff0c090fb3
frankkib <frankkibagendi13@gmail.com>
2023-02-06 09:27:11 +0000

    function that check if the instance is true of false

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Is kind of class defination"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    returns the instance of an object
+    """
+
+    return (isinstance(obj, a_class))
