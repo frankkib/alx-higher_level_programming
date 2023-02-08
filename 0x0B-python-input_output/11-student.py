520a18f41581478d7c02784080bc313c87ff8b4f
frankkib <frankkibagendi13@gmail.com>
2023-02-08 09:15:15 +0000

    function that replaces attributes

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""class defination"""
+
+
+class Student:
+    """class initialization"""
+    def __init__(self, first_name, last_name, age):
+        """representaion"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """returns dictionary representaion of student"""
+        return self.__dict__
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        new = {}
+        for num in attrs:
+            try:
+                new[num] = self.__dict__[num]
+            except Exception as Error:
+                pass
+            return new
+
+    def reload_from_json(self, json):
+        """replaces atrributes"""
+        for key, index in json.items():
+            setatrr(self, key, index)
