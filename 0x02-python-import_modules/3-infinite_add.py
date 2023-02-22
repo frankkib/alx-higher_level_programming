f0098a8690d50b31ac9cc75a59ed3ef4c169c00c
frankkib <frankkibagendi13@gmail.com>
2023-02-22 10:47:55 +0000

    fixed the pycode errors

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,6 +2,8 @@
 if __name__ == "__main__":
     import sys
     sum_total = 0
-    for i in range(len(sys.argv) - 1):
+    for i in range(len(sys.argviii)
+
+):
         sum_total += int(sys.argv[i + 1])
         print("{}".format(sum_total))
