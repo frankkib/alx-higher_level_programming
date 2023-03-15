a8e74c0be13a966c6885d40d93f9d3dd6001d414
frankkib <frankkibagendi13@gmail.com>
2023-03-15 03:19:16 +0000

    a script that creates a database

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,4 @@
--- script that creates the database hbtn_0c_0 in your MySQL server.
+-- script that creates the database
+-- hbtn_0c_0 in your MySQL server.
 
 CREATE DATABSE IF NOT EXISTS hbtn_0c_0;
