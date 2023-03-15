7e10c3a30d1c49d1a27525507a72864fe3495811
frankkib <frankkibagendi13@gmail.com>
2023-03-15 11:36:42 +0000

    a script that databse and user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,5 @@
---A  script that creates the MySQL server user user_0d_1.
+--A  script that creates the database hbtn_0d_2 and the user user_0d_2.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2pwd';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON hbtn_0d_.* TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
