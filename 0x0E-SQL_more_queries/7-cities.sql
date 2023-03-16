f51c6e983d714f17c4ba2a84e78197327641ced0
frankkib <frankkibagendi13@gmail.com>
2023-03-16 12:01:22 +0000

    fixed the cities id

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,4 +1,4 @@
---A script that creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa) on your MySQL server.
+-- A script that creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa) on your MySQL server.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
