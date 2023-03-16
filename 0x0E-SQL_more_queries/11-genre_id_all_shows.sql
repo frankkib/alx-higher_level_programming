4403b7b66feb85fbb0a8cd081dc840808ba73061
frankkib <frankkibagendi13@gmail.com>
2023-03-16 12:41:38 +0000

    A script that lists all shows contained in hbtn_0d_tvshows without a genre linked

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,4 +1,4 @@
--- A  script that lists all shows contained in hbtn_0d_tvshows without a genre linked.
+-- A script that lists all shows contained in hbtn_0d_tvshows.
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres
