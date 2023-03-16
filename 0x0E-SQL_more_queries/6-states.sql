d556391da4b9e89d9c497867daf9f422fc93c6b4
frankkib <frankkibagendi13@gmail.com>
2023-03-16 11:58:23 +0000

    fixed unknown database error

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,5 +1,5 @@
---Ascript that creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on your MySQL server
-CREATE DATBASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS  hbtn_0d_us.states(
+-- Ascript that creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on your MySQL server
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS  hbtn_0d_usa.states(
 	id INT NOT NULL  PRIMARY KEY AUTO_INCREMENT, 
-	name VARCHAR(256) NOT NULL,);
+	name VARCHAR(256) NOT NULL);
