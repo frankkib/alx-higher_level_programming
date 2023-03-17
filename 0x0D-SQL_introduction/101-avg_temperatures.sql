176c6318d0c8046e450ec5d7d7db7d02f5bc8796
frankkib <frankkibagendi13@gmail.com>
2023-03-17 06:10:03 +0000

    removed the unknown column

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,2 +1,2 @@
 -- A script that displays the average temperature (Fahrenheit) by city ordered by temperature (descending).
-SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY temperatures DESC;
+SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city DESC;
