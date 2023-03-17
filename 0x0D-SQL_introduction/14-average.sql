8f971fb5de5f350dde5fa0683fe691504af978e3
frankkib <frankkibagendi13@gmail.com>
2023-03-17 04:09:16 +0000

    fixed syntax error

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,2 +1,2 @@
---A script that computes the score average of all records in the table second_table of the database hbtn_0c_0 in your MySQL server.
+-- Ascript that computes the score average of all records in the table second_table of the database hbtn_0c_0 in your MySQL server.
 SELECT AVG(socre) AS avarage FROM second_table;
