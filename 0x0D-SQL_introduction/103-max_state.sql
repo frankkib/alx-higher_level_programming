648b0e5fa0a83e924ea174f90a8030a0a3fe2307
frankkib <frankkibagendi13@gmail.com>
2023-03-17 06:04:45 +0000

    fixed the syntax error

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,4 +1,4 @@
---A script that displays the max temperature of each state (ordered by State name)
+-- A script that displays the max temperature of each state (ordered by State name)
 SELECT state, max(value) as max_temp
 FROM temperatures
 ORDER BY state
