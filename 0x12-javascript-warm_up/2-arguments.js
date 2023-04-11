3f44482227e828c0ab2df3dbddf3b434c3430788
frankkib <frankkibagendi13@gmail.com>
2023-04-11 11:24:32 +0000

    script printing no arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const argCount = process.argv.length - 2;
+
+if (argCount === 0) {
+	console.log('No argument');
+}  else if (argCount === 2) {
+	console.log('Argument found');
+}  else {
+	console.log('Arguments found');
+}
+
