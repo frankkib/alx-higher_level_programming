107a270e06d79bafa16414efacaffc19ef78cfd5
frankkib <frankkibagendi13@gmail.com>
2023-04-11 11:07:23 +0000

    a script that prints multiple lines

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const line1Var = 'C is fun';
+const line2Var = 'Python is cool';
+const line3Var = 'JavaScript is amazing';
+console.log(line1Var);
+console.log(line2Var);
+console.log(line3Var);
