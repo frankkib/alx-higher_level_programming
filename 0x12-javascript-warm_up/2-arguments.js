d7948a775b64b86f21424f4773469dbe41ca0f59
frankkib <frankkibagendi13@gmail.com>
2023-04-11 16:34:18 +0000

    fixed the semistandard errors

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,10 +3,9 @@
 const argCount = process.argv.length - 2;
 
 if (argCount === 0) {
-	console.log('No argument');
-}  else if (argCount === 2) {
-	console.log('Argument found');
-}  else {
-	console.log('Arguments found');
+  console.log('No argument');
+} else if (argCount === 2) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
-
