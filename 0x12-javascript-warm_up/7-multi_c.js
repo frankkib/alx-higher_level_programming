75d22130b9263b91b726d73953916957321c90c9
frankkib <frankkibagendi13@gmail.com>
2023-04-12 10:46:56 +0000

    printing a string a number of arguments

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const args = process.argv[2];
+const x = args[0];
+if (isNaN(x)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
+}
