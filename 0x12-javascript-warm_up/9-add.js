bcbd1ad1da805b270d5554cab6548fbd0a659c99
frankkib <frankkibagendi13@gmail.com>
2023-04-12 11:27:25 +0000

    addit two arguments

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -6,7 +6,7 @@ function add (a, b) {
 const arg1 = parseInt(process.argv[2]);
 const arg2 = parseInt(process.argv[3]);
 if (isNaN(arg1) || isNaN(arg2)) {
-  console.log('Invalid arguments');
+  console.log('NaN');
 } else {
   console.log(add(arg1, arg2));
 }
