54cc73aec5a6a81888ae03cff1604c0fd93bd3b2
frankkib <frankkibagendi13@gmail.com>
2023-04-13 10:38:20 +0000

    a class that defines square and inherits from ractangle

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+module.exports = class Square extends require('./5-square.js') {
+  charPrint (c = 'X') {
+    for (let i = 0; i < this.width; i++) {
+      console.log(c.repeat(this.height));
+    }
+  }
+};
