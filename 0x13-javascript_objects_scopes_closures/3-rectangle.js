68f6baeb9de495dd210dda4bf57bed9f5d767f31
frankkib <frankkibagendi13@gmail.com>
2023-04-13 09:14:47 +0000

    creating class raectangle and printing a rectangle

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+      return Object.create(null);
+    }
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    if (!this.width || !this.height) {
+      return;
+    }
+
+    let rect = '';
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        rect += 'X';
+      }
+      rect += '\n';
+    }
+
+    console.log(rect);
+  }
+};
