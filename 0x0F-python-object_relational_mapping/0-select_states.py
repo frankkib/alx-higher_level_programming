a1773738b18b590d3af6c03bc90ad0fe76f729ea
frankkib <frankkibagendi13@gmail.com>
2023-04-17 06:59:09 +0000

    fixed documantion error

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,13 +6,14 @@ hbtn_0e_0_usa
 
 import MySQLdb
 import sys
-
-
-def list():
+if __name__ = "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
 
     db = MySQLdb.connect(
-            host='localhost', port=3306, user=sys.argv[1],
-            passwd=sys.argv[2], db=sys.argv[3])
+            host='localhost', port=3306, user=sys.username,
+            passwd=password, db=db_name)
 
     cursor = db.cursor()
 
@@ -24,7 +25,3 @@ def list():
         print(state)
     cursor.close()
     db.close()
-
-
-if __name__ = "__main__":
-    list()
