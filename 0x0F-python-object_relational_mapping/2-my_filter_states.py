aba847f8cdebea820fa4713e8ff508d7d1fa5a90
frankkib <frankkibagendi13@gmail.com>
2023-04-17 10:28:27 +0000

    A script that accepts an argument

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/pytthon3
+"""A script that takes argument and displays the states"""
+import MySQLdb
+import sys
+if __name__ = "__main__":
+    username = sys.agrv[1]
+    password = sys.argv[2]
+    database_name= sys.argv[3]
+    state_name = sys.argv[4]
+
+    db = MySQLdb.connect(host="localhost", port=3306,
+            user=username, passwd=password
+            db=database_name)
+
+    cur =db.cursor()
+
+    cur.execute("SELECT * FROM states WHERE name=%s ORDER BY id ASC", (state_name))
+    
+    for city in cur.fetchall():
+        print(city)
+    cur.close()
+    db.close()
