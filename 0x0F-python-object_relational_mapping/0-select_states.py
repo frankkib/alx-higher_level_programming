f381af60171522f6880aa093b57ef7a70a84c57c
frankkib <frankkibagendi13@gmail.com>
2023-04-18 03:47:26 +0000

    fixed documentation error

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,27 +1,23 @@
 #!/usr/bin/python3
-"""
-a script for printing all states in the database
-hbtn_0e_0_usa
-"""
+"""A script that filters the database"""
 
 import MySQLdb
 import sys
-if __name__ = "__main__":
+
+if __name__ == '__main__':
     username = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
 
     db = MySQLdb.connect(
-            host='localhost', port=3306, user=username,
+            host="localhost", port=3306, user=username,
             passwd=password, db=db_name)
 
-    cursor = db.cursor()
+    cur = db.cursor()
 
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
 
-    states = cursor.fetchall()
+    for row in cur.fetchall():
+        print(row)
 
-    for state in states:
-        print(state)
-    cursor.close()
     db.close()
