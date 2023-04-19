93406df20f820473dda8bbeb1147b9ff824447ed
frankkib <frankkibagendi13@gmail.com>
2023-04-19 06:01:17 +0000

    fixed the module error

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -3,18 +3,18 @@
 import MySQLdb
 import sys
 if __name__ == "__main__":
-    username = sys.agrv[1]
+    username = sys.argv[1]
     password = sys.argv[2]
     database_name= sys.argv[3]
     state_name = sys.argv[4]
 
     db = MySQLdb.connect(host="localhost", port=3306,
-            user=username, passwd=password
+            user=username, passwd=password,
             db=database_name)
 
     cur =db.cursor()
 
-    cur.execute("SELECT * FROM states WHERE name=%s ORDER BY id ASC", (state_name))
+    cur.execute("SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY id ASC".format(state_name))
     
     for city in cur.fetchall():
         print(city)
