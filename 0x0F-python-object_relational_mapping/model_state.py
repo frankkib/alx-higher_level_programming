b4dfe07bc8b5e86418dadd7de8cae3502eed93bc
frankkib <frankkibagendi13@gmail.com>
2023-04-19 08:25:47 +0000

    a python file that contains the class definition of a State and an instance

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
+
 """module that contains class defination"""
+
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
+
 Base = declarative_base()
 
+
 class State(Base):
+
     """class state defination"""
+
     __table__ = 'states'
 
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False, unique=True)
+    id = Column(
+            Integer, primary_key=True, autoincrement=True, nullable=False, unique=True)
     name = Column(String(128), nullable=False)
     
     def __repr__(self):
