2b083c1b37a53bb04173c4e829557b461a064671
frankkib <frankkibagendi13@gmail.com>
2023-04-19 12:46:55 +0000

    script for add new city cities

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,16 @@
+#!usr/bin/pythyon3
+"""linking a class"""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from model_state import Base
+
+
+class City(Base):
+    """class defination"""
+
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, nullable=True,
+            autoincrement=True, unique=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    state = relationship("State", backref="cities")
