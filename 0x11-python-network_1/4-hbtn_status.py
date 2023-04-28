70fa53dca0ff3615f404fb291906cd8307f913cf
frankkib <frankkibagendi13@gmail.com>
2023-04-28 09:54:13 +0000

    a pythons scripts for getting request

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,4 +7,4 @@ if __name__ == "__main__":
     response = requests.get(url)
     print("Body response:")
     print("\t- type:", type(response.text))
-    print("\t- type:", response.text)
+    print("\t- content:", response.text)
