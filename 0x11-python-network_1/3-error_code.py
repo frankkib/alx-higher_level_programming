7495899dc9b3a528d73423bd0b8e55b2a8eaf3ea
frankkib <frankkibagendi13@gmail.com>
2023-04-28 09:33:16 +0000

    python scripts that fetches requests

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -7,8 +7,8 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     try:
-        urllib.request.urlopen(url) as response:
-            print(response.read().decode('utf-8'))
-    except url.error.HTTPError as e:
-        print(e.code)
+        response = urllib.request.urlopen(url)
+        print(response.read().decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print(f"Error code: {e.code}")
 
