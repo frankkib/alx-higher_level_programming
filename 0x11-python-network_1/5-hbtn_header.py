e9126d9cbb6e5d0519586e69cea7ca71a631084a
frankkib <frankkibagendi13@gmail.com>
2023-04-28 10:02:47 +0000

    Python script that takes in a URL, sends a request to the URL and displays the value of the variable

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -7,4 +7,4 @@ if __name__ == "__main__":
     url = sys.argv[1]
     response = requests.get(url)
     request_id = response.headers.get('X-Request-Id')
-    print( request_id)
+    print(request_id)
