aec7102b706341275eddbc20e93aab5ef709c93c
frankkib <frankkibagendi13@gmail.com>
2023-04-28 09:52:09 +0000

    a pythons scripts for getting request

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""A Python script that fetches"""
+import requests
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    response = requests.get(url)
+    print("Body response:")
+    print("\t- type:", type(response.text))
+    print("\t- type:", response.text)
