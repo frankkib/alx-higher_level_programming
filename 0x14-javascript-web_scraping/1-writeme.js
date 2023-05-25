c19269f2df2de5973cfdc2800e42bbe6ee824b1e
frankkib <frankkibagendi13@gmail.com>
2023-05-25 08:16:28 +0000

    removed successfull from the else statement

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,7 +4,6 @@ const fs = require('fs');
 const [, , filePath, fileContent] = process.argv;
 
 if (!filePath || !fileContent) {
-  console.log('Please provide the file path and the string content');
   process.exit(1);
 }
 fs.writeFile(filePath, fileContent, 'utf-8', (err) => {
