390a5742218ddb0eb27b5017534005aca8175125
frankkib <frankkibagendi13@gmail.com>
2023-05-25 08:14:36 +0000

    removed successfull from the else statement

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -11,7 +11,5 @@ fs.writeFile(filePath, fileContent, 'utf-8', (err) => {
   if (err) {
     console.log('An error occurred while writing to the file:');
     console.log(err);
-  } else {
-    console.log('Susccessfull');
   }
 });
